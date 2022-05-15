c72e1495121baced8a6a45b3ab27e618f22ee952
Denisa Dud <denisa.dud@student.upt.ro>
2022-05-15 16:34:37 +0300

    Am schimbat pom-ul si am facut fxml urile

---
+++ src/main/java/sample/DButils.java
@@ -0,0 +1,46 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+
+public class DButils {
+    public static void changeScene(ActionEvent event, String fxmlFile, String title,String username) {
+        Parent root = null;
+        if(username != null ) {
+            try {
+                FXMLLoader loader = new FXMLLoader(DButils.class.getResource(fxmlFile));
+                root = loader.load();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            try {
+                root = FXMLLoader.load(DButils.class.getResource(fxmlFile));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        Stage stage=(Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setTitle(title);
+        stage.setScene(new Scene(root, 400,300));
+        stage.show();
+    }
+   /* public static void singUpUser(ActionEvent event, String username, String password):
+    Connection connection=null;
+    PreparedStatment psInsert=null;
+    PreparedStatment psCheckUserExists=null;
+     ResultSet resultSet=null;
+try{
+    connection=DriverManager.getConnection("")
+    }
+
+*/
+}
---
+++ src/main/java/sample/LoggedInController.java
@@ -0,0 +1,27 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import java.util.ResourceBundle;
+import java.net.URL;
+
+public class LoggedInController implements Initializable {
+
+    @FXML
+    private Button button_logout;
+    @FXML
+    private Label loggedin;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        button_logout.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+
+            }
+        });
+    }}
