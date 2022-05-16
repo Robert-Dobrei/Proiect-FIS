92d4ef53925f1a638318072050e694e95c4851ca
Robert <crazylord2001@gmail.com>
2022-05-16 18:13:44 +0300

    small

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.proiectfis {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.proiectfis to javafx.fxml;
-    exports com.example.proiectfis;
-}
